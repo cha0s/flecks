2ac981c9e1a7224338b9f2600cdbedd896852b79
cha0s <git@cha0s.io>
2022-02-26 06:28:03 -0600

    chore: error

--- packages/user/src/session/server.js
+++ packages/user/src/session/server.js
@@ -15,7 +15,11 @@ export default {
       const urle = express.urlencoded({extended: true});
       return (req, res, next) => {
         debug('@flecks/http/server/request.route: express.urlencoded()');
-        urle(req, res, () => {
+        urle(req, res, (error) => {
+          if (error) {
+            next(error);
+            return;
+          }
           debug('@flecks/http/server/request.route: session()');
           flecks.get('$flecks/user.session')(req, res, (error) => {
             if (error) {
