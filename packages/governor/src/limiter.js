0a8c66ad7d6da5d63563c40c509f5a165146d7be
cha0s <git@cha0s.io>
2022-03-01 14:26:33 -0600

    fix: old redis

--- packages/governor/src/limiter.js
+++ packages/governor/src/limiter.js
@@ -2,7 +2,11 @@ import {createClient} from 'redis';
 import {RateLimiterRedis} from 'rate-limiter-flexible';
 
 export default async (flecks, options) => {
-  const storeClient = createClient();
+  const {
+    host,
+    port,
+  } = flecks.get('@flecks/redis/server');
+  const storeClient = createClient({host, port});
   // @todo node-redis@4
   // await storeClient.connect();
   return new RateLimiterRedis({
