668304408db0266aa4e2cd9ea34e8d1e5063a2dc
cha0s <git@cha0s.io>
2022-03-11 08:08:36 -0600

    chore: assumptions

--- packages/core/src/server/build/babel.config.js
+++ packages/core/src/server/build/babel.config.js
@@ -1,6 +1,18 @@
 module.exports = (api) => {
   api.cache(true);
   return {
+    assumptions: {
+      constantSuper: true,
+      ignoreFunctionLength: true,
+      noClassCalls: true,
+      noDocumentAll: true,
+      objectRestNoSymbols: true,
+      privateFieldsAsProperties: true,
+      setClassMethods: true,
+      setComputedProperties: true,
+      setPublicClassFields: true,
+      setSpreadProperties: true,
+    },
     plugins: [
       '@babel/plugin-proposal-optional-chaining',
     ],
