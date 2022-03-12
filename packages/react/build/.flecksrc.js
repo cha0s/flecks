dd37fa69ab409aaaf1bb74df044f7edac11f1b58
cha0s <git@cha0s.io>
2022-03-12 09:11:43 -0600

    refactor: http handles style loading

--- packages/react/build/.flecksrc.js
+++ packages/react/build/.flecksrc.js
@@ -1,13 +1,8 @@
-const {join} = require('path');
-
 module.exports = {
   aliases: {
     'react-dom': '@hot-loader/react-dom',
   },
   babel: {
-    plugins: [
-      join(__dirname, '..', 'style-loader'),
-    ],
     presets: [
       '@babel/preset-react',
     ],
