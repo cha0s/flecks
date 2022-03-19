754646d4a1df63de44f91e5a3184ebd9c75afd95
cha0s <git@cha0s.io>
2022-03-19 17:44:35 -0500

    feat(react): configure in web's absence

--- packages/react/src/server.js
+++ packages/react/src/server.js
@@ -1,10 +1,11 @@
 import {Hooks} from '@flecks/core';
+import {augmentBuild} from '@flecks/web/server';
 
 import ssr from './ssr';
 
 export default {
   [Hooks]: {
-    '@flecks/core.build': (target, config) => {
+    '@flecks/core.build': (target, config, flecks) => {
       // Resolution.
       config.use.push(({config}) => {
         config.resolve.alias
@@ -13,6 +14,12 @@ export default {
           .prepend('.web.js')
           .prepend('.web.jsx');
       });
+      // Augment the build on behalf of a missing `@flecks/web`.
+      if (!flecks.fleck('@flecks/web/server')) {
+        flecks.registerBuildConfig('postcss.config.js', {fleck: '@flecks/web/server'});
+        flecks.registerResolver('@flecks/web');
+        augmentBuild(target, config, flecks);
+      }
     },
     '@flecks/web/server.stream.html': (stream, req, flecks) => (
       flecks.get('@flecks/react.ssr') ? ssr(stream, req, flecks) : stream
