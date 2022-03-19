c58bf6583029bbc7ff250a4a63b96429171ac165
cha0s <git@cha0s.io>
2022-03-19 15:26:07 -0500

    refactor: bit more to the login form

--- packages/user/src/local/client/components/user-local-login.jsx
+++ packages/user/src/local/client/components/user-local-login.jsx
@@ -2,9 +2,24 @@ import {React} from '@flecks/react';
 
 const UserLocalLogin = () => (
   <form action="/auth/local" method="post">
-    <input name="email" />
-    <input name="password" />
-    <input type="submit" />
+    <label>
+      <span>Email address</span>
+      <input name="email" type="text" />
+    </label>
+    <label>
+      <span>Password</span>
+      <input name="password" type="password" />
+    </label>
+    <div>
+      <label>
+        <input type="checkbox" />
+        <span>Remember me</span>
+      </label>
+      <a href="/auth/forgot">Forgot password?</a>
+    </div>
+    <label>
+      <input type="submit" value="Sign in" />
+    </label>
   </form>
 );
 
