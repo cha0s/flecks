d4a0e43aa4147fb2e802eaa88c8508d2785048a6
cha0s <git@cha0s.io>
2022-03-29 21:02:05 -0500

    refactor: no async generator

--- packages/core/src/server/build/babel.config.js
+++ packages/core/src/server/build/babel.config.js
@@ -18,6 +18,7 @@ module.exports = (api) => {
         {
           exclude: [
             '@babel/plugin-transform-regenerator',
+            '@babel/plugin-transform-async-to-generator',
           ],
         },
       ],
