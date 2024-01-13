84a30df67ab0c8b5c34bb61657f6103775839e82
cha0s <git@cha0s.io>
2024-01-13 04:46:07 -0600

    feat(fleck): ProcessAssets

--- packages/fleck/src/server/build/fleck.webpack.config.js
+++ packages/fleck/src/server/build/fleck.webpack.config.js
@@ -1,7 +1,10 @@
 const flecksConfigFn = require('@flecks/core/server/build/webpack.config');
 
+const ProcessAssets = require('./process-assets');
+
 module.exports = async (env, argv, flecks) => {
   const config = await flecksConfigFn(env, argv, flecks);
+  config.plugins.push(new ProcessAssets(flecks));
   config.stats = flecks.get('@flecks/fleck/server.stats');
   return config;
 };
--- packages/fleck/src/server/index.js
+++ packages/fleck/src/server/index.js
@@ -1,4 +1,12 @@
+import {join} from 'path';
+
+import {glob} from '@flecks/core/server';
+
 import commands from './commands';
+
+const {
+  FLECKS_CORE_ROOT = process.cwd(),
+} = process.env;
 
 export const hooks = {
   '@flecks/core.commands': commands,
@@ -12,4 +20,32 @@ export const hooks = {
     },
   }),
   '@flecks/core.targets': () => ['fleck'],
+  '@flecks/fleck/server.processAssets': async (assets, compilation, flecks) => {
+    const {RawSource} = compilation.compiler.webpack.sources;
+    const packageJson = assets['package.json'];
+    const json = JSON.parse(packageJson.source().toString());
+    const {files} = json;
+    // Add defaults.
+    files.push('build', 'src');
+    // Add tests if they exist.
+    const testFiles = await glob(join(FLECKS_CORE_ROOT, 'test/*.js'));
+    if (testFiles.length > 0) {
+      files.push('test', 'test.js');
+    }
+    // Let others have a say.
+    await flecks.invokeSequentialAsync('@flecks/fleck/server.packageJson', json, compilation);
+    // Add any sourcemaps.
+    json.files = json.files
+      .map((filename) => {
+        const maybeWithMap = [filename];
+        if (compilation.assets[`${filename}.map`]) {
+          maybeWithMap.push(`${filename}.map`);
+        }
+        return maybeWithMap;
+      })
+      .flat();
+    // Sort and uniquify.
+    json.files = [...new Set(json.files.sort((l, r) => (l < r ? -1 : 1)))];
+    compilation.updateAsset('package.json', new RawSource(JSON.stringify(json, null, 2)));
+  },
 };
