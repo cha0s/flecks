6d1c59b3517b527f436aadd322ddad7a47eab9d6
cha0s <git@cha0s.io>
2024-01-27 06:49:54 -0600

    perf: enhanced-resolve is far too slow for runtime

--- packages/build/build/resolve.js
+++ packages/build/build/resolve.js
@@ -1,24 +1,70 @@
+const {resolve: resolvePath} = require('path');
+
 const D = require('@flecks/core/build/debug');
 
 const debug = D('@flecks/server/build/runtime');
 
-const Resolver = require('./resolver');
-
-module.exports = function resolve(resolverConfig, stubs) {
+module.exports = function resolve({alias, fallback}, stubs) {
   debug('installing resolution magic');
   const {Module} = require('module');
   const {require: Mr} = Module.prototype;
-  const resolver = new Resolver({
-    ...resolverConfig,
-    modules: [],
-    useSyncFileSystemCalls: true,
-  });
+  function preprocessMatchers(matchers) {
+    return Object.entries(matchers)
+      .map(([from, to]) => {
+        if (false === to) {
+          stubs.push(from);
+          return undefined;
+        }
+        if (from.endsWith('$')) {
+          return {
+            exact: true,
+            from: from.slice(0, -1),
+            to,
+          };
+        }
+        return {
+          exact: false,
+          from,
+          to,
+        };
+      })
+      .filter((entry) => entry);
+  }
+  function match(path, request, matchers) {
+    for (let i = 0; i < matchers.length; ++i) {
+      const {exact, from, to} = matchers[i];
+      const resolvedFrom = resolvePath(path, from);
+      const resolvedTo = resolvePath(path, to);
+      if (exact) {
+        if (resolvedFrom === request) {
+          return request.replace(resolvedFrom, resolvedTo);
+        }
+      }
+      else if (request.startsWith(resolvedFrom)) {
+        return request.replace(resolvedFrom, resolvedTo);
+      }
+    }
+    return undefined;
+  }
+  const aliases = preprocessMatchers(alias, stubs);
+  const fallbacks = preprocessMatchers(fallback, stubs);
   Module.prototype.require = function flecksResolutionMagic(request, options) {
     for (let i = 0; i < stubs.length; ++i) {
       if (request.startsWith(stubs[i])) {
         return undefined;
       }
     }
-    return Mr.call(this, resolver.resolveSync(request) || request, options);
+    const qualified = resolvePath(this.path, request);
+    let resolved = match(this.path, qualified, aliases);
+    if (!resolved) {
+      resolved = match(this.path, qualified, fallbacks);
+    }
+    if (!resolved) {
+      resolved = request;
+    }
+    return Mr.call(this, resolved, options);
+  };
+  return () => {
+    Module.prototype.require = Mr;
   };
 };
