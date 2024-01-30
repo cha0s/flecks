74c1a8559d62f46f86d1e0e3ad1647e83c2297c2
cha0s <git@cha0s.io>
2024-01-30 09:48:33 -0600

    chore: revert (more broken than I thought)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -32,33 +32,6 @@ export default async function flecksDocusaurus() {
       }],
     ],
     projectName: 'flecks', // Usually your repo name.
-    ssrTemplate: `<!DOCTYPE html>
-    <html data-theme="dark" <%~ it.htmlAttributes %>>
-      <head>
-        <meta charset="UTF-8">
-        <meta name="generator" content="Docusaurus v<%= it.version %>">
-        <% it.metaAttributes.forEach((metaAttribute) => { %>
-          <%~ metaAttribute %>
-        <% }); %>
-        <%~ it.headTags %>
-        <% it.stylesheets.forEach((stylesheet) => { %>
-          <link rel="stylesheet" href="<%= it.baseUrl %><%= stylesheet %>" />
-        <% }); %>
-        <% it.scripts.forEach((script) => { %>
-          <link rel="preload" href="<%= it.baseUrl %><%= script %>" as="script">
-        <% }); %>
-      </head>
-      <body <%~ it.bodyAttributes %>>
-        <%~ it.preBodyTags %>
-        <div id="__docusaurus">
-          <%~ it.appHtml %>
-        </div>
-        <% it.scripts.forEach((script) => { %>
-          <script src="<%= it.baseUrl %><%= script %>"></script>
-        <% }); %>
-        <%~ it.postBodyTags %>
-      </body>
-    </html>`,
     tagline: 'not static',
     themes: ['@docusaurus/theme-mermaid'],
     themeConfig:
