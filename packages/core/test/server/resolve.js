ddfe7f6522928be303a061612b0cf8fffe2bfcf9
cha0s <git@cha0s.io>
2024-02-04 00:25:57 -0600

    ci: don't test module cache

--- packages/core/test/server/resolve.js
+++ packages/core/test/server/resolve.js
@@ -22,10 +22,6 @@ it('can resolve inexact', async () => {
   expect(__non_webpack_require__(join(root, 'blah')))
     .to.equal(4);
   clear();
-  delete __non_webpack_require__.cache['/home/cha0s/sync/src/code/flecks/packages/core/test/server/resolve/blah.js'];
-  expect(__non_webpack_require__(join(root, 'blah')))
-    .to.equal(3);
-  delete __non_webpack_require__.cache['/home/cha0s/sync/src/code/flecks/packages/core/test/server/resolve/blah.js'];
 });
 
 it('can resolve exact', async () => {
@@ -35,16 +31,9 @@ it('can resolve exact', async () => {
     },
     fallback: {},
   }, []);
-  expect(__non_webpack_require__(join(root, 'blah')))
-    .to.equal(3);
   expect(__non_webpack_require__(join(root, 'boo')))
     .to.equal(2);
   clear();
-  delete __non_webpack_require__.cache['/home/cha0s/sync/src/code/flecks/packages/core/test/server/resolve/blah.js'];
-  delete __non_webpack_require__.cache['/home/cha0s/sync/src/code/flecks/packages/core/test/server/resolve/boo.js'];
-  expect(__non_webpack_require__(join(root, 'boo')))
-    .to.equal(1);
-  delete __non_webpack_require__.cache['/home/cha0s/sync/src/code/flecks/packages/core/test/server/resolve/boo.js'];
 });
 
 it('can resolve false', async () => {
@@ -54,10 +43,7 @@ it('can resolve false', async () => {
     },
     fallback: {},
   }, []);
-  expect(__non_webpack_require__(join(root, 'boo.js')))
+  expect(__non_webpack_require__(join(root, 'moo')))
     .to.be.undefined;
   clear();
-  delete __non_webpack_require__.cache['/home/cha0s/sync/src/code/flecks/packages/core/test/server/resolve/boo.js'];
-  expect(__non_webpack_require__(join(root, 'boo.js')))
-    .to.equal(1);
 });
