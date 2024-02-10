1eff7f4a65ec916c6f24dd5424a18d9f336d7e86
cha0s <git@cha0s.io>
2024-02-10 11:09:21 -0600

    refactor: post-lerna

--- packages/build/build/build.test.webpack.config.js
+++
@@ -1,8 +0,0 @@
-const Build = require('./build');
-const configFn = require('./test.webpack.config');
-
-module.exports = async (env, argv) => {
-  const flecks = await Build.from();
-  const config = await configFn(env, argv, flecks);
-  return config;
-};
