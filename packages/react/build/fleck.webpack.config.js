5a75b284713211cc98d3e7a247221ba5d47b09d1
cha0s <git@cha0s.io>
2024-02-18 08:36:07 -0600

    fix: self-externalization for build

--- packages/react/build/fleck.webpack.config.js
+++ packages/react/build/fleck.webpack.config.js
@@ -1,10 +1,20 @@
+const {join} = require('path');
+
 const {externals} = require('@flecks/build/src/server');
 const configFn = require('@flecks/fleck/build/fleck.webpack.config');
 
+const {
+  FLECKS_CORE_ROOT = process.cwd(),
+} = process.env;
+
 module.exports = async (env, argv, flecks) => {
   const config = await configFn(env, argv, flecks);
+  const {name} = require(join(FLECKS_CORE_ROOT, 'package.json'));
   config.externals = await externals({
-    allowlist: ['react-tabs/style/react-tabs.css'],
+    allowlist: [
+      new RegExp(`^${name}`),
+      'react-tabs/style/react-tabs.css',
+    ],
   });
   return config;
 };
--- packages/web/build/fleck.webpack.config.js
+++ packages/web/build/fleck.webpack.config.js
@@ -1,13 +1,23 @@
+const {join} = require('path');
+
 const {copy, externals} = require('@flecks/build/src/server');
 const configFn = require('@flecks/fleck/build/fleck.webpack.config');
+
+const {
+  FLECKS_CORE_ROOT = process.cwd(),
+} = process.env;
 
 module.exports = async (env, argv, flecks) => {
   const config = await configFn(env, argv, flecks);
   delete config.entry['server/build/entry'];
   delete config.entry['server/build/template'];
   delete config.entry['server/build/tests'];
+  const {name} = require(join(FLECKS_CORE_ROOT, 'package.json'));
   config.externals = await externals({
-    allowlist: ['mocha/mocha.css'],
+    allowlist: [
+      new RegExp(`^${name}`),
+      'mocha/mocha.css',
+    ],
   });
   config.plugins.push(
     copy({
